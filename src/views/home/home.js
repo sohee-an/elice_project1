c857705be40dc549f07e85b089e171fc16366589
Danheon <eksgjs3847@gmail.com>
2022-05-24 14:31:45 +0900

    사이드 메뉴 적용

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -2,6 +2,7 @@
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다.
 
+import { sidebar } from '../sidebar/sidebar.js';
 import * as Api from '/api.js';
 import { randomId } from '/useful-functions.js';
 
@@ -11,6 +12,7 @@ const greetingDiv = document.querySelector('#greetingDiv');
 
 addAllElements();
 addAllEvents();
+sidebar();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 async function addAllElements() {
