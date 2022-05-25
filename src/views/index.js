aca59291b09db631045c2b915417389a7eb48194
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-05-25 14:37:53 +0900

    상품 목록 페이지 사이드 바 구현

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,8 @@
 // products.html과 연결
+import { sidebar } from './sidebar/sidebar.js';
 import Products_screen from "./products_screen.js";
 
+sidebar();
 const routes = {
   "/": Products_screen,
   "/product/:product_id": Products_screen,
