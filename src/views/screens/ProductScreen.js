01300a97001886700c393b8413b264e79567045e
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-05-26 12:21:45 +0900

    frontend/api

--- src/views/screens/ProductScreen.js
+++ src/views/screens/ProductScreen.js
@@ -1,7 +1,14 @@
+import {parseRequestUrl} from '../utils.js'
+import { getProduct } from '../api.js';
 
 const ProductScreen = {
-  render:()=>{
-    return `<div>ProductScreen</div>`
+  render: async () => {
+    const request = parseRequestUrl();
+    const product = await getProduct(request.id);
+    if (product.error) {
+      return `<div>${product.error}</div>`;
+    }
+    return `<h1>${product.name}</h1>`;
   },
 };
 
