565d3a07c0ca23c783c2d3ac2a1e0bfef0d103a1
Danheon <eksgjs3847@gmail.com>
2022-05-27 23:40:37 +0900

    [프론트엔드] user-update 기능 구현 (진행중)

--- client/src/views/user-delete/user-delete.js
+++ client/src/views/user-delete/user-delete.js
@@ -5,18 +5,32 @@ changeNavbar();
 handleLogoutBtn();
 
 const submitBtn = document.querySelector(".form-box .button");
-const input = document.querySelector(".form-box .input");
+const passwordInput = document.querySelector(".form-box .input");
 const modal = document.querySelector(".modal");
+const yesBtn = document.querySelector('#deleteCompleteBtn');
+const closeBtn = document.querySelectorAll('.close');
 
-// submit 버튼 클릭시
-// 모달창 classList.add('is-active')
-
-// 모달창 yes 버튼 클릭 시
-// 비밀번호를 전달해주고
-// 틀리면 -> alert
-// 맞으면 -> alert, db에서 회원정보 삭제
+// 회원정보 삭제 버튼 클릭시 모달창 띄움
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.classList.add('is-active');
+})
 
 // 모달창 닫기
-// 아니오 버튼
-// background
-// x버튼
+closeBtn.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.remove('is-active');
+  })
+})
+
+// 모달창 yes 버튼 클릭 시
+// yesBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   if (passwordInput.value === password) {
+//     alert('회원 정보가 삭제되었습니다.');
+//     // db에서 회원정보 삭제
+//   } else {
+//     alert('비밀번호가 일치하지 않습니다.');
+//   }
+// })
