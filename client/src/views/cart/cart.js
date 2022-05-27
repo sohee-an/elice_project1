53a2913d2dd620c9f51780962724093d684116e4
Yeonjin <ygeenee@naver.com>
2022-05-27 18:23:23 +0900

    [프론트] cart - 구매하기 버튼 기능 구현 (FEAT)

--- client/src/views/cart/cart.js
+++ client/src/views/cart/cart.js
@@ -2,6 +2,7 @@ import { addCommas, convertToNumber } from '/useful-functions.js';
 import * as Api from '/api.js'
 
 const itemListElem = document.getElementById("item-list");
+const purchaseBtnElem = document.getElementById("purchase-btn");
 
 /** Needed
  * local storage와 연결 필요
@@ -23,6 +24,7 @@ const items = [{
 
 createItemList();
 updateItemList();
+deleteItem();
 
 
 // 아이템 렌더링을 위한 div 생성
@@ -36,7 +38,7 @@ function createItemList() {
     if(items.length==0) newItems=`<li>장바구니가 비어있습니다.</li>`;
     else newItems = items.reduce((acc, cur) => { return acc + ` <li id="item${cur.item}">
         <div class="item"> 
-            <div> <input type="checkbox" checked/> </div>
+            <div> <input type="checkbox" class="checked" id="checkbox-${cur._id}" checked/> </div>
             
             <input type="image" class="item-img" src="${cur.image}">
             <div class="item-info">
@@ -67,14 +69,14 @@ function updateItemList() {
         const inputElem = elem.querySelector('.quantity-input');
         const elem_id=inputElem.id.split('-')[1];   // 해당 아이템 아이디
 
-        minusElem.addEventListener("click", async () =>{
+        minusElem.addEventListener("click", () =>{
             if(inputElem.value > 1){   // 아이템이 1개 이상일 때 수량 감소가능
                 items.find( e => e._id==elem_id ).quantity--;
                 inputElem.value--;
                 getPaymentInfo();
             } 
         })
-        plusElem.addEventListener("click", async () =>{
+        plusElem.addEventListener("click", () =>{
             if(inputElem.value < 100){   // 아이템이 100개 이하일 때 수량 증가가능
                 items.find( e => e._id==elem_id ).quantity++;
                 inputElem.value++;
@@ -85,7 +87,30 @@ function updateItemList() {
 }
 
 function deleteItem() {
-    
+    const selectAllElem = document.querySelector('#select-all');
+    const deleteAllElem = document.querySelector('#delete-all');
+    const deleteSelectedElem = document.querySelector('#delete-selected');
+    const allItemElem = document.querySelectorAll('.checked');
+
+    // [ ] 체크박스 클릭 시
+    selectAllElem.addEventListener('click', ()=> {
+        allItemElem.forEach(e=>{
+            e.checked=selectAllElem.checked;
+        }) 
+    })
+
+    // 모든 상품 삭제시 -> 경고 창
+    deleteAllElem.addEventListener('click', ()=> {
+        if(confirm("장바구니의 모든 상품을 삭제합니다.")){
+            items=[];
+            itemListElem.innerHTML=`장바구니가 비어있습니다.`;
+            getPaymentInfo();
+        }
+    })
+
+    // 선택한 상품 삭제
+
+
 }
 
 // 결제 정보 보여주기 
@@ -110,9 +135,50 @@ function getPaymentInfo() {
     totalElem.innerText=addCommas(totalPrice); 
 }
 
-function changeQuantity(btn_type, id){
-    const item_id=id.split('-')[1];
-    if(btn_type=='+'){ 
-        console.log(item_id);
+async function purchaseBtn(e) {
+    e.preventDefault();
+  
+    if(localStorage.getItem('token')){  // 로그인 되어있는 경우
+
     }
-}
+    else {
+        alert('구매를 위해서는 로그인해야 합니다.')
+    }
+  
+    // 잘 입력했는지 확인
+    const isEmailValid = validateEmail(email);
+    const isPasswordValid = password.length >= 4;
+  
+    if (!isEmailValid || !isPasswordValid) {
+      return alert(
+        '비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요.'
+      );
+    }
+  
+  
+    // 로그인 api 요청
+    try {
+      const data = { email, password };
+  
+      const result = await Api.post('/api/login', data);
+      const token = result.token;
+      const role = result.role;
+  
+      // 로그인 성공, 토큰을 세션 스토리지에 저장
+      // 물론 다른 스토리지여도 됨
+      localStorage.setItem('token', token);
+      localStorage.setItem('role', role);
+  
+      alert(`정상적으로 로그인되었습니다.`);
+      // 로그인 성공
+  
+      // TODO: db와 로그인 폼 입력값 비교해서 맞으면 로그인 성공
+      // TODO: 관리자 계정
+  
+      // 기본 페이지로 이동
+      window.location.href = '/';
+    } catch (err) {
+      console.error(err.stack);
+      alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+    }
+  }
