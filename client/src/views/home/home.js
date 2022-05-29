3e374eaa2e7896bee45ac13a338acec9c98e3609
[SW2/서울/1반] 박재현 <jaehyeonp74@gmail.com>
2022-05-30 01:56:16 +0900

    [프론트]-home-메인페이지-DOCS

--- client/src/views/home/home.js
+++ client/src/views/home/home.js
@@ -3,24 +3,3 @@ import { changeNavbar, handleLogoutBtn } from '../common/navbar/navbar.js';
 sidebar();
 changeNavbar();
 handleLogoutBtn();
-
-// 메인페이지에서 이미지에 마우스 갖다 댔을 때!
-
-let shopmenDiv = document.querySelector('#shopmenDiv');
-let shopwomenDiv = document.querySelector('#shopwomenDiv');
-let hidden = shopmenDiv.querySelector('.hidden');
-let hidden1 = shopwomenDiv.querySelector('.hidden');
-
-shopmenDiv.addEventListener('mouseenter', (e) => {
-  hidden.classList.remove('hidden');
-})
-shopmenDiv.addEventListener('mouseleave', (e) => {
-  hidden.classList.add('hidden');
-})
-
-shopwomenDiv.addEventListener('mouseenter', (e) => {
-  hidden1.classList.remove('hidden');
-})
-shopwomenDiv.addEventListener('mouseleave', (e) => {
-  hidden1.classList.add('hidden');
-})
