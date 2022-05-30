5d6f5c1352dfb00cb3fb60801a1fd6a895242125
[SW2/서울/2반] 서경환 <seogyeonghwan48@gmail.com>
2022-05-30 19:16:50 +0900

    [백엔드] - 전체적으로 자잘한 오류 수정

--- client/src/views/order/order.js
+++ client/src/views/order/order.js
@@ -28,13 +28,13 @@ function getPaymentInfo() {
 
     let itemAmount = items.reduce((acc, cur) => acc + Number(cur.quantity), 0);
     let itemPrice = items.reduce((acc, cur) => acc + Number((cur.price * cur.quantity)), 0);
-    let shippingPrice = itemPrice? 3000:0;
+    let shippingPrice = itemPrice ? 3000 : 0;
     let totalPrice = itemPrice + shippingPrice;
 
-    amountElem.innerText = addCommas(itemAmount)+'개';
-    priceElem.innerText = '$'+addCommas(itemPrice);
-    shippingElem.innerText = '$'+addCommas(shippingPrice);
-    totalElem.innerText = '$'+addCommas(totalPrice);
+    amountElem.innerText = addCommas(itemAmount) + '개';
+    priceElem.innerText = '$' + addCommas(itemPrice);
+    shippingElem.innerText = '$' + addCommas(shippingPrice);
+    totalElem.innerText = '$' + addCommas(totalPrice);
 }
 
 
@@ -47,47 +47,47 @@ async function handleSubmit(e) {
     e.preventDefault();
 
     const name = document.querySelector('#d-name').value;
-    const phoneNumber= document.querySelector('#d-phoneNumber').value;
+    const phoneNumber = document.querySelector('#d-phoneNumber').value;
     const postcode = document.querySelector('#sample4_postcode').value;
     const address = document.querySelector('#sample4_roadAddress').value;
     const detailAddress = document.querySelector('#sample4_detailAddress').value;
     const orderRequest = document.querySelector('#d-requests').value;
 
     console.log(orderRequest);
-    if(!localStorage.getItem('token')){ // 로그인 안되어있을 경우
+    if (!localStorage.getItem('token')) { // 로그인 안되어있을 경우
         window.location = '/login';
         return;
     }
 
     if (!name || !phoneNumber || !postcode || !detailAddress || !address) { // 주문 정보 필드 입력이 완료되지 않았을 경우
-        
-      return alert(`주문 정보를 입력해주세요.`);
+
+        return alert(`주문 정보를 입력해주세요.`);
     }
-    
-    if (getCartItems().length==0) {   // 장바구니에 물건이 담겨있지 않는 경우
+
+    if (getCartItems().length == 0) {   // 장바구니에 물건이 담겨있지 않는 경우
         alert(`구매할 제품이 없습니다. 제품을 선택해주세요.`);
-        window.location.href ='/products';
+        window.location.href = '/products';
         return;
     }
-   
-    try {
-        const cartItems = getCartItems().filter(e=>{ return {productId: e.id, quantity: e.quantity}; });
 
-        const data = { 
+    try {
+        const cartItems = getCartItems().filter(e => { return { productId: e.id, quantity: e.quantity }; });
+
+        const data = {
             name,
             phoneNumber,
-            address : {
+            address: {
                 postalCode: postcode,
                 address1: address,
                 address2: detailAddress,
             },
             orderRequest,
-            cartItems : cartItems
+            cartItems: cartItems
         };
 
 
         console.log(data);
-        
+
         await Api.post('/api/order', data);
 
         alert(`주문 및 결제가 완료되었습니다.`);
@@ -95,8 +95,8 @@ async function handleSubmit(e) {
         // 로그인 페이지 이동
         window.location.href = '/complete';
     } catch (err) {
-      console.error(err.stack);
-      alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+        console.error(err.stack);
+        alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
     }
 }
-  
+
